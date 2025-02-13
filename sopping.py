6b5f09513b6cc937ef73f58bc1d93d5b0d05a770
c2acsbal <c2acsbal@vasvari.org>
2025-02-13 11:14:33 +0100

    hanynap függvény megalkotása és kiírása

--- sopping.py
+++ sopping.py
@@ -1,1 +1,19 @@
-def behivo():
+f = open("vasarlas.csv", "r", encoding="utf-8")
+
+for tartalom in f:
+    tartalom = tartalom.split(";")
+
+def hanynap(tartalom):
+    napszam = 0
+    for i in tartalom:
+       napszam += 1
+    
+    return napszam
+
+napszamveg = hanynap(tartalom)
+
+print(napszamveg)
+
+
+
+    
