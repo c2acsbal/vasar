bf3a7cd167202beb3d9530c52d6e4599fe94af05
c2acsbal <c2acsbal@vasvari.org>
2025-02-13 11:22:22 +0100

    koltesmentes függvény megalkotása és kiírása

--- sopping.py
+++ sopping.py
@@ -1,7 +1,7 @@
 f = open("vasarlas.csv", "r", encoding="utf-8")
 
 for tartalom in f:
-    tartalom = tartalom.split(";")
+    tartalom = tartalom.strip().split(";")
 
 def hanynap(tartalom):
     napszam = 0
@@ -16,4 +16,15 @@ print(napszamveg)
 
 
 
+def koltesmentes(tartalom):
+    napszam = 0
+    for i in tartalom:
+        if int(i) == 0:
+            napszam +=1
+    
+    return (napszam)
+
+napszamvegkoltsegmentes = koltesmentes(tartalom)
+
+print(napszamvegkoltsegmentes)
     
